a22588721a959f1225b24993b6f72dd97256a3d1
Dan Morriss <danielmorriss1@gmail.com>
2023-12-19 17:48:30 +0000

    Use setState for handleClick

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,16 +7,30 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello",
             buttonText: "Exit",
+            count: 0,
         };
     }
 
     handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter",
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }
         });
-        console.log(this.state.introduction)
 
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State:', prevState)
+            console.log('Previous Props:', prevProps)
+            return {
+                count: prevState.count + 1
+            }
+        });
     }
     
     render() {
@@ -24,6 +38,8 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
