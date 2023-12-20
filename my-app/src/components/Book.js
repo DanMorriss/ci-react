a9693c122b92985aa184e96f7a32d4e4963d5d31
Dan Morriss <danielmorriss1@gmail.com>
2023-12-20 22:14:08 +0000

    Add key prop to map lists

--- my-app/src/components/Book.js
+++ my-app/src/components/Book.js
@@ -4,11 +4,9 @@ function Book(props) {
     const book = props.book
     return (
         <div>
-            <div>
-                <h5>{book.title}</h5>
-                <p>{book.author}</p>
-                <p>{book.pages}</p>
-            </div>
+            <h5>{book.title}</h5>
+            <p>{book.author}</p>
+            <p>{book.pages}</p>
         </div>
     )
 }
