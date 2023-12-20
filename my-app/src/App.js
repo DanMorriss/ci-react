1637cbf5c1736a1b1dedb56084dae4f72f1b8998
Dan Morriss <danielmorriss1@gmail.com>
2023-12-20 14:00:59 +0000

    Add es7 snippets and new click events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
-      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike" />
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,59 @@
+    // rfce
+    //  Gives you this...
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+    // rafce
+    // Same thing but as an arrow function...
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+    // rce
+    // Class component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+    // rconst
+    // to create the constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      
+//   }
+// }
+
+
+    // clg
+    // to log something to the console
+//console.log()
+
+    // clo
+    // to log an object and its name to the console
+// console.log('first', first)
