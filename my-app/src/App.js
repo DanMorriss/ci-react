69d3d85a9eddf4648e71f64915d38b03cbe43478
Dan Morriss <danielmorriss1@gmail.com>
2024-01-03 16:15:07 +0000

    Add SearchBar.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import UncontrolledForms from "./components/UncontrolledForms";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return <div className="App">
-      <UncontrolledForms />
+      <SearchBar />
   </div>;
 }
 
