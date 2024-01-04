dc5e62615c0e89740ee8b4cdb61ab880ff22dcd1
Dan Morriss <danielmorriss1@gmail.com>
2024-01-04 13:08:26 +0000

    Add cleanup function to UseEffectCounter

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -7,16 +7,22 @@ function UseEffectCounter() {
     // The useEffect runs after ever render. 
     // The [count] after the arrow function is to specify it should run after the [count] is updated.
     useEffect(() => {
-        console.log('count 1 effect')
+        console.log('count 1 effect');
         document.title = count;
     }, [count]);
     // To run it only once, pass the second parameter as an empty array.
     useEffect(() => {
-        console.log('Creating timer')
+        console.log('Creating timer');
         const interval = setInterval(() => {
+            console.log('Interval executed');
             setTime(time => time +1)
-        }, 1000)
-    }, [])
+        }, 1000);
+        // Run the cleanup function  once you've finished with it so the DOM isn't keeping track of unneeded variables.
+        return () => {
+            console.log('Cleaning up');
+            clearInterval(interval);
+        }
+    }, []);
 
     return (
     <div>
